908152dcd7f62b84e9d10f70de941e6907ceacea
Hamza Shafiq <hamza.shafiq@square63.com>
2017-05-25 12:45:16 +0500

    User: Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'Hamza', age: 22, role: 'admin', status: 'enabled')
