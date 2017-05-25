6a8f6bc5f26ccb0134ce946cdba67b763df2bff5
Hamza Shafiq <hamza.shafiq@square63.com>
2017-05-25 12:28:45 +0500

    Created model for user and team and added associations

---
+++ db/migrate/20170525072704_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :age
+      t.double :average
+      t.string :role
+      t.string :status
+      t.integer :matches
+      t.references :team, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
