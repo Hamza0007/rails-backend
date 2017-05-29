1f3e9e6e4d22c96d7d5617ce45f808463470a5e3
Hamza Shafiq <hamza.shafiq@square63.com>
2017-05-29 17:19:50 +0500

    User: Implemented CRUD functionality for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,27 +1,41 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :set_user, only: [:show, :update]
+  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_team, only: :update
 
   def create
-    begin
-      User.create(user_params)
-
-      render json: { result: 'User Created Successfully'}, status: :ok
-    rescue => e
-      render json: { result: 'Invalid params'}, status: :bad_request
+    @user = User.new(user_params)
+    @user.team_id = user_params['team_id'].blank? ? nil : user_params['team_id'].to_i
+    if @user.save
+      return render json: { result: 'User Created Successfully'}, status: :ok
+    else
+      render json: { result: 'Invalid params', errors: @user.errors}, status: :bad_request
     end
   end
 
   def index
-    @users = User.all
-    render json: @users.to_json(only: [:name, :age, :average, :role, :status, :matches, :team_id])
+    @users = User.player.uniq
+    render json: @users, include: { team: {only: [:id, :name, :country]}}, except: [:created_at, :updated_at]
   end
 
   def show
-    render json: @team.to_json(only: [:name, :age, :average, :role, :status, :matches, :team_id])
+    render json: @users, include: { team: {only: [:id, :name, :country]}}, except: [:created_at, :updated_at]
   end
 
   def update
+    if @user.update(user_params)
+      return render json: { result: 'User Updated Successfully'}, status: :ok
+    else
+      render json: { result: 'Invalid params', errors: @user.errors}, status: :bad_request
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: { result: 'User Deleted Successfully'}, status: :ok
+    else
+      render json: { result: 'Invalid params'}, status: :bad_request
+    end
   end
 
   private
@@ -31,6 +45,10 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :age, :average, :role, :status, :matches, :team_id)
+      params.permit(:name, :age, :average, :matches, :team_id)
+    end
+
+    def set_team
+      @user.team_id = user_params['team_id'].blank? ? nil : user_params['team_id'].to_i
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :team
+
+  scope :admin, -> { where(role: 'admin') }
+  scope :player, -> { where(role: 'player') }
 end
