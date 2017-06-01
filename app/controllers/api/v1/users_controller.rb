0fcc76f1eaf8b479d2474df2cb60cf104e681ce9
Hamza Shafiq <hamza.shafiq@square63.com>
2017-06-01 14:30:26 +0500

    User: Adeed authentication for user, added image and fixed email issues

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :validate_user
   before_action :set_user, only: [:show, :update, :destroy]
   before_action :set_team, only: :update
 
   def create
     @user = User.new(user_params)
     @user.team_id = user_params['team_id'].blank? ? nil : user_params['team_id'].to_i
-    if @user.save
+    if @user.save(validate: false)
       return render json: { result: 'User Created Successfully'}, status: :ok
     else
       render json: { result: 'Invalid params', errors: @user.errors}, status: :bad_request
@@ -23,7 +24,8 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
+    @user.assign_attributes(user_params)
+    if @user.save(validate: false)
       return render json: { result: 'User Updated Successfully'}, status: :ok
     else
       render json: { result: 'Invalid params', errors: @user.errors}, status: :bad_request
@@ -45,10 +47,15 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def user_params
-      params.permit(:name, :age, :average, :matches, :team_id)
+      params.permit(:name, :age, :average, :matches, :team_id, :image)
     end
 
     def set_team
       @user.team_id = user_params['team_id'].blank? ? nil : user_params['team_id'].to_i
+    end
+
+    def validate_user
+      @user = User.find_by_email(request.headers['HTTP_UID'])
+      return render json: { result: 'Invalid user'}, status: :unauthorized if @user.blank? || @user.tokens[request.headers['HTTP_CLIENT']].blank?
     end
 end
